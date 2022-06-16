971ced6e61630ea9afabda2840150a572475ae33
MohammadTaghi (Sherv) Elmi <mohammad.taghi.elmi@gmail.com>
2022-06-16 18:57:46 +0300

    Delete e2e packages boilerplates

--- packages/dashboard-e2e/src/integration/menuButton/menuButton.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('dashboard: NavMenuButton component', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=navmenubutton--primary'));
-    
-    it('should render the component', () => {
-      cy.get('h1').should('contain', 'Welcome to NavMenuButton!');
-    });
-});
